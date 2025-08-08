f7586de6a9a6a1a060708c26cb4cadf651fc9ce2
Mario Torres <mariotorresdev@gmail.com>
2025-08-08 16:06:43 -0500

    fix: added more env vars and sql logging

--- Database/ConnectionManager.php
+++ Database/ConnectionManager.php
@@ -1,17 +1,18 @@
 <?php
 
-  function GetLocalMySQLConnection()
-  {
-     $hostname = (!empty(getenv("MYSQL_SERVER_NAME")) ? getenv("MYSQL_SERVER_NAME") : "localhost");
-     $username = (!empty(getenv("MYSQL_SERVER_USER_NAME")) ? getenv("MYSQL_SERVER_USER_NAME") : "root");
-     $password = (!empty(getenv("MYSQL_ROOT_PASSWORD")) ? getenv("MYSQL_ROOT_PASSWORD") : "");
-     $database = "swuonline";
-	   try {
-       return mysqli_connect($hostname, $username, $password, $database);
-     } catch (\Exception $e) {
+function GetLocalMySQLConnection()
+{
+  $hostname = (!empty(getenv("MYSQL_SERVER_NAME")) ? getenv("MYSQL_SERVER_NAME") : "localhost");
+  $username = (!empty(getenv("MYSQL_SERVER_USER_NAME")) ? getenv("MYSQL_SERVER_USER_NAME") : "root");
+  $password = (!empty(getenv("MYSQL_ROOT_PASSWORD")) ? getenv("MYSQL_ROOT_PASSWORD") : "");
+  $database = (!empty(getenv("MYSQL_DATABASE_NAME")) ? getenv("MYSQL_DATABASE_NAME") : "swuonline");
 
-     }
-     return false;
-   }
+  $conn = mysqli_connect($hostname, $username, $password, $database);
+  if (!$conn) {
+    error_log("MySQL Connection error: " . mysqli_connect_error());
+    return false;
+  }
 
- ?>
+  return $conn;
+}
+?>
