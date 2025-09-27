a892b81430da7f6d549f2e873929dffef0fc7ebd
OotTheMonk <nathanandrecoder@gmail.com>
2025-09-27 14:05:08 -0500

    VIsual fix

--- Stats/DeckMetaStats.php
+++ Stats/DeckMetaStats.php
@@ -205,7 +205,7 @@ $forIndividual = false;
       for (var i=0; i<json.length; ++i) {
         var r = json[i];
         rows += '<tr>';
-        rows += '<td style="white-space:nowrap;display:flex;gap:6px;align-items:stretch;">'
+        rows += '<td style="white-space:nowrap;gap:6px;align-items:stretch;">'
           + '<a href="https://swustats.net/TCGEngine/Stats/Decks.php?leader=' + r.leaderID + '&base=' + r.baseID + '">' 
           + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">'
           + '<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>'
