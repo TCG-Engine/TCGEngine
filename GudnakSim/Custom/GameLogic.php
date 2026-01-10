119709d5b7d25ed7da808f525c5af4da281b7b72
OotTheMonk <nathanandrecoder@gmail.com>
2026-01-10 17:27:54 -0600

    Gloaming Basics

--- GudnakSim/Custom/GameLogic.php
+++ GudnakSim/Custom/GameLogic.php
@@ -377,16 +377,19 @@ function CurrentCardPower($fromZone, $destZone, $isAttacker=false) {
     //Self power modifiers
     switch($thisCard->CardID) {
         case "RYBF1DSKH": case "RYBF2DSKH": case "RYBF3DSKH": //Dusklight Hunter
+        case "GMBF1SPCH": case "GMBF2SPCH": case "GMBF3SPCH": //Spectral Hunter
             if($isAttacker && TraitContains($destTop, "Brute")) {
                 $totalPower += 1;
             }
             break;
         case "RYBF1DWNB": case "RYBF2DWNB": case "RYBF3DWNB": //Dawnbringer Brute
+        case "GMBF1AMBT": case "GMBF2AMBT": case "GMBF3AMBT": //Amalgam Brute
             if($isAttacker && TraitContains($destTop, "Soldier")) {
                 $totalPower += 1;
             }
             break;
         case "RYBF1SLSD": case "RYBF2SLSD": case "RYBF3SLSD": //Solaran Soldier
+        case "GMBF1SKLS": case "GMBF2SKLS": case "GMBF3SKLS": //Spectral Soldier
             if($isAttacker && TraitContains($destTop, "Hunter")) {
                 $totalPower += 1;
             }
