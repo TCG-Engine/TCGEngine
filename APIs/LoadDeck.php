51aba1efabd138402daa29b8853e51270ddf090a
OotTheMonk <nathanandrecoder@gmail.com>
2026-03-03 10:32:15 -0600

    Add CORS header

--- APIs/LoadDeck.php
+++ APIs/LoadDeck.php
@@ -1,5 +1,10 @@
 <?php
 
+  // CORS headers for public API
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+  header("Access-Control-Allow-Headers: Content-Type");
+
   require_once "../Core/HTTPLibraries.php";
 
   $response = new stdClass();
